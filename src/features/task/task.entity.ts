5bc6b63f070cff414c387f2ccd4f34ffcbb3be75
Shoun Ichida <shoun@ichida.fr>
2016-06-17 21:56:45 +0200

    Adding entity task with start/stop feature

--- src/features/task/task.entity.ts
+++ src/features/task/task.entity.ts
@@ -2,5 +2,20 @@
  * This class represents a Task
  */
 export class Task {
+    constructor(public startTime: Date = null, public endTime: Date = null) {
+    }
 
+    start(): void {
+        if (null != this.startTime) {
+            throw new Error('Cannot start a task that already started');
+        }
+        this.startTime = new Date();
+    }
+
+    stop(): void {
+        if (null == this.startTime) {
+            throw new Error('Cannot stop a task that didn\'t start');
+        }
+        this.endTime = new Date();
+    }
 }
--- src/features/task/task.spec.ts
+++ src/features/task/task.spec.ts
@@ -4,4 +4,38 @@ describe('Task', () => {
   it('should be instanciable', () => {
     expect(new Task()).toBeDefined();
   });
+  it('should have a null startTime and a null endTime', () => {
+    const task: Task = new Task();
+    expect(task.startTime).toBeDefined();
+    expect(task.endTime).toBeDefined();
+    expect(task.startTime).toBeNull();
+    expect(task.endTime).toBeNull();
+  });
+  it('should be able to start itself', () => {
+    const task: Task = new Task();
+    task.start();
+    expect(task.startTime).not.toBeNull();
+  });
+  it('should be able to stop itself', () => {
+    const task: Task = new Task();
+    task.start();
+    expect(task.startTime).not.toBeNull();
+    expect(task.endTime).toBeNull();
+    task.stop();
+    expect(task.startTime).not.toBeNull();
+    expect(task.endTime).not.toBeNull();
+  });
+  it('should\'t be able to stop if not started', () => {
+    const task: Task = new Task();
+    expect(task.stop.bind(task)).toThrow(new Error('Cannot stop a task that didn\'t start'));
+  });
+  it('should\'t be able to start if already started', () => {
+    const task: Task = new Task();
+    task.start();
+    expect(task.start.bind(task)).toThrow(new Error('Cannot start a task that already started'));
+    const task2: Task = new Task();
+    task2.start();
+    task2.stop();
+    expect(task2.start.bind(task)).toThrow(new Error('Cannot start a task that already started'));
+  });
 });
